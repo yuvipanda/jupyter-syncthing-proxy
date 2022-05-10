4d7eeab9f3b12ffed5e0608d97020f49d2058cfe
YuviPanda <yuvipanda@gmail.com>
2022-05-10 11:43:27 +0530

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="jupyter-syncthing-proxy",
-    version='1.0.2',
+    version='1.0.3',
     url="https://github.com/yuvipanda/jupyter-syncthing-proxy",
     author="Yuvi Panda",
     description="yuvipanda@gmail.com",
